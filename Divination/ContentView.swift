9ba312ae8a45c632e252b291cf4c330d9c134879
sannma31 <kasaisyouun2004@gmail.com>
2023-12-04 11:58:40 +0900

    add:名前と血液型のTextFieldの追加

--- Divination/ContentView.swift
+++ Divination/ContentView.swift
@@ -4,16 +4,28 @@
 //
 //  Created by 笠井翔雲 on 2023/12/04.
 //
-
 import SwiftUI
 
+struct YearMonthDay: Hashable {
+    var year: Int
+    var month: Int
+    var day: Int
+}
+
 struct ContentView: View {
+    @State private var name: String = "ゆめみん"
+    @State private var birthday: YearMonthDay = YearMonthDay(year: 2000, month: 1, day: 27)
+    @State private var bloodType: String = "ab"
+    @State private var today: YearMonthDay = YearMonthDay(year: 2000, month: 1, day: 27)
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("名前を入力してください", text: $name)
+                .padding(10)
+
+            
+            TextField("血液型を入力してください", text: $bloodType)
+                .padding(10)
         }
         .padding()
     }
