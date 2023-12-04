ef31270ea7f5d6dbbb07fa755773feaed83202b3
sannma31 <kasaisyouun2004@gmail.com>
2023-12-04 14:21:00 +0900

    add:DataPickerの追加、textの追加

--- Divination/ContentView.swift
+++ Divination/ContentView.swift
@@ -17,17 +17,47 @@ struct ContentView: View {
     @State private var birthday: YearMonthDay = YearMonthDay(year: 2000, month: 1, day: 27)
     @State private var bloodType: String = "ab"
     @State private var today: YearMonthDay = YearMonthDay(year: 2000, month: 1, day: 27)
-
+    
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
+            Text("名前を入力してね")
+                .font(.headline)
+            
             TextField("名前を入力してください", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(10)
-
+                .foregroundColor(.gray)
+            
+            Text("誕生日を入力してください")
+                .font(.headline)
+            
+            DatePicker("", selection: $birthday.date, displayedComponents: .date)
+                .datePickerStyle(DefaultDatePickerStyle())
+                .labelsHidden()
+                .padding(10)
+            
+            Text("血液型を入力してください")
+                .font(.headline)
             
             TextField("血液型を入力してください", text: $bloodType)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(10)
         }
         .padding()
+    }
+}
+
+extension YearMonthDay {
+    var date: Date {
+        get {
+            return Calendar.current.date(from: DateComponents(year: year, month: month, day: day)) ?? Date()
+        }
+        set {
+            let components = Calendar.current.dateComponents([.year, .month, .day], from: newValue)
+            year = components.year ?? 0
+            month = components.month ?? 0
+            day = components.day ?? 0
+        }
     }
 }
 
@@ -36,3 +66,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
